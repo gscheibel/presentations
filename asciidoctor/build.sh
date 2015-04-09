0e30e213d10583af434fd0a07be133c32cd88e25
Guillaume Scheibel <gscheibel@expedia.com>
2015-04-09 13:19:16 +0200

    DevoxxFR version

--- asciidoctor/build.sh
+++ asciidoctor/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 rm main.html
+rm stylesheets/devoxx.css
+cd reveal.js
+grunt sass
+cp css/theme/devoxx.css ../stylesheets/
+cd ..
 asciidoctor -a config=reveal -r asciidoctor-diagram -T asciidoctor-reveal.js/templates/slim/ main.adoc
-asciidoctor-pdf -a config=pdf -r asciidoctor-diagram  main.adoc
+
+#rm main.pdf
+#asciidoctor-pdf -a config=pdf -r asciidoctor-diagram  main.adoc
